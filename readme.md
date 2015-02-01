6cc38d36131b376b0f7a793a60985024e9bb58b4
Vlad Badelita <vladbadelita@gmail.com>
2015-02-01 23:02:33 +0200

    Organized a bit

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 #Kennie
+
+Platformer graphics by Kenney Vleugels (www.kenney.nl)
+Built using Phaser API (www.phaser.io)
--- src/game/main.js
+++ src/game/main.js
@@ -15,50 +15,11 @@ var state = {
     cursors: null,
     scoreText: null,
     score: 0,
-    frameCounter: 0,
+    updateCounter: 0,
 
-
-    collectCoin: function(player, coin) {
-        coin.kill();
-        this.score += 10;
-        scoreText.text = 'Score: ' + this.score;
-    },
-
-    dropScore: function(player, critter) {
-        player.x = 32;
-        player.y = game.world.height - 160;   
-        this.score = 0;
-        scoreText.text = 'Score: ' + this.score;
-    },
-
-    isPlayerOnTop: function(player, critter) {
-        if (critter.dead) return false;
-        if (player.y + player.height - 10 < critter.y) {
-            return true;
-        }
-        return false;
-    },
-
-    killCritter: function(player, critter) {
-        // critter.kill();
-        critter.dead = true;
-        critter.animations.play('dead');
-        critter.body.gravity.y = 600;
-        this.score += 100;
-        scoreText.text = 'Score: ' + this.score;
-    },
-
-    preload: function() {
-        game.stage.backgroundColor = '#80E0ED'
-        game.load.image('ground', '../assets/ground.png');
-        game.load.image('coinGold', '../assets/coinGold.png');
-        game.load.atlasJSONHash('slime', '../assets/slime.png', '../assets/slime.json');
-        game.load.atlasJSONHash('hero', '../assets/hero.png', '../assets/hero.json');
-        game.load.atlasJSONHash('fly', '../assets/fly.png', '../assets/fly.json');
-    },
-    create: function(){
+    initWorld: function() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        game.world.setBounds(0, 0, 5000, 640);
+        game.world.setBounds(0, 0, 5000, 940);
 
         platforms = game.add.group();
         platforms.enableBody = true;
@@ -67,18 +28,20 @@ var state = {
         ground.scale.setTo(100, 2);
         ground.body.immovable = true;
 
-        var ledge = platforms.create(400, 400, 'ground');
+        var ledge = platforms.create(500, game.world.height - 250, 'ground');
         ledge.scale.setTo(10, 1);
         ledge.body.immovable = true;
 
-        ledge = platforms.create(-150, 200, 'ground');
+        ledge = platforms.create(-150, game.world.height - 450, 'ground');
         ledge.scale.setTo(7,1);
         ledge.body.immovable = true;
 
-        ledge = platforms.create(1300, 300, 'ground');
+        ledge = platforms.create(1300, game.world.height - 500, 'ground');
         ledge.scale.setTo(13,1);
         ledge.body.immovable = true;
+    },
 
+    initCoins: function() {
         coins = game.add.group();
         coins.enableBody = true;
 
@@ -89,7 +52,9 @@ var state = {
             coin.body.bounce.y = 0.5 + Math.random() * 0.2;
             coin.body.collideWorldBounds = true;
         }
+    },
 
+    initPlayer: function() {
         player = game.add.sprite(32, game.world.height - 160, 'hero', 'front');
         game.physics.arcade.enable(player);
         player.body.bounce.y = 0.0;
@@ -105,13 +70,15 @@ var state = {
         player.animations.add('jumpRight', ['jumpRight'], 1, true);
 
         game.camera.follow(player);
+    },
 
+    initCritters: function() {
         slimes = game.add.group();
         slimes.enableBody = true;
 
         var slime;
         for (i = 0; i < 20; i += 1) {
-            slime = slimes.create(i * 300 + Math.random() * 200, 200, 'slime');
+            slime = slimes.create(i * 300 + Math.random() * 200, game.world.height - 500, 'slime');
             slime.body.gravity.y = 300;
             slime.body.bounce.y = 0;
             slime.body.collideWorldBounds = true;
@@ -127,7 +94,7 @@ var state = {
 
         var fly;
         for (i = 0; i < 10; i += 1) {
-            fly = flies.create(i * 500 + Math.random() * 200, 200, 'fly', 'flyLeft1');
+            fly = flies.create(i * 500 + Math.random() * 200, game.world.height - 500 * Math.random(), 'fly', 'flyLeft1');
             fly.body.bounce.y = 0;
             fly.body.collideWorldBounds = true;
             fly.animations.add('flyLeft', ['flyLeft1', 'flyLeft2'], 10, true);
@@ -135,31 +102,61 @@ var state = {
             fly.animations.add('dead', ['flyDead'], 1, true);
             fly.dead = false;
         }
-
-
-        cursors = game.input.keyboard.createCursorKeys();
-
-        scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-        scoreText.fixedToCamera = true;
     },
-    update: function() {
+
+    collectCoin: function(player, coin) {
+        coin.kill();
+        this.score += 10;
+        scoreText.text = 'Score: ' + this.score;
+    },
+
+    killHero: function(player) {
+        player.x = 32;
+        player.y = game.world.height - 180;   
+        this.score = 0;
+        scoreText.text = 'Score: ' + this.score;
+    },
+
+    isOnTop: function(e1, e2) {
+        if (e1.y + e1.height - 10 < e2.y) {
+            return true;
+        }
+        return false;
+    },
+
+    killCritter: function(player, critter) {
+        critter.dead = true;
+        critter.body.velocity.x = 0;
+        critter.body.velocity.y = 0;
+        critter.body.gravity.y = 10000;
+
+        critter.animations.play('dead');
+        game.time.events.add(Phaser.Timer.SECOND * 5, critter.destroy, critter);
+
+        this.score += 100;
+        scoreText.text = 'Score: ' + this.score;
+    },
+
+    handleCollisions: function() {
         game.physics.arcade.collide(player, platforms);
         game.physics.arcade.collide(coins, platforms);
         game.physics.arcade.overlap(player, coins, this.collectCoin, null, this);
         game.physics.arcade.collide(slimes, platforms);
         game.physics.arcade.collide(flies, platforms);
-        game.physics.arcade.overlap(player, flies, this.killCritter, this.isPlayerOnTop, this);
-        game.physics.arcade.overlap(player, flies, this.dropScore, function(player, critter) {
+        game.physics.arcade.overlap(player, flies, this.killCritter, this.isOnTop, this);
+        game.physics.arcade.overlap(player, flies, this.killHero, function(player, critter) {
                 if (critter.dead) return false;
-                return (!this.isPlayerOnTop(player, critter));
+                return (!this.isOnTop(player, critter));
             }, this);
 
-        game.physics.arcade.overlap(player, slimes, this.killCritter, this.isPlayerOnTop, this);
-        game.physics.arcade.overlap(player, slimes, this.dropScore, function(player, critter) {
+        game.physics.arcade.overlap(player, slimes, this.killCritter, this.isOnTop, this);
+        game.physics.arcade.overlap(player, slimes, this.killHero, function(player, critter) {
                 if (critter.dead) return false;
-                return (!this.isPlayerOnTop(player, critter));
+                return (!this.isOnTop(player, critter));
             }, this);
+    },
 
+    handlePlayerInput: function() {
         player.body.velocity.x = 0;
 
         if (cursors.left.isDown) {
@@ -187,43 +184,70 @@ var state = {
             player.animations.play('jumpRight');
             player.body.velocity.y = -500;
         }
+    },
 
-        slimes.forEachAlive(function(slime, time) {
-            slime.body.velocity.x = 0;
-            if (slime.dead) return;
-            if (time % 300 < 150) {
-                slime.body.velocity.x = 150;
-                slime.animations.play('moveRight');
-            } else {
-                slime.body.velocity.x = -150;
-                slime.animations.play('moveLeft');
-            }
-        }, this, this.frameCounter);
+    animateSlime: function(slime, time) {
+        slime.body.velocity.x = 0;
+        if (slime.dead) return;
+        if (time % 300 < 150) {
+            slime.body.velocity.x = 150;
+            slime.animations.play('moveRight');
+        } else {
+            slime.body.velocity.x = -150;
+            slime.animations.play('moveLeft');
+        }
+    },
 
-        flies.forEachAlive(function(fly, time) {
-            fly.body.velocity.x = 0;
+    animateFly: function(fly, time) {
+        fly.body.velocity.x = 0;
+        fly.body.velocity.y = 0;
+        if (fly.dead) return;
+
+        var magic = time % 500;
+        if (magic  < 250) {
+            fly.body.velocity.x = 150;
+            fly.animations.play('flyRight');
+        } else {
+            fly.body.velocity.x = -150;
+            fly.animations.play('flyLeft');
+        }
+        if (magic * Math.random() < 20) {
+            fly.body.velocity.y = -150;
+        } else if (magic * Math.random() < 40) {
+            fly.body.velocity.y = 150;
+        } else {
             fly.body.velocity.y = 0;
-            if (fly.dead) return;
-            var magic = (time % 500);
-            if (magic  < 250) {
-                fly.body.velocity.x = 150;
-                fly.animations.play('flyRight');
-            } else {
-                fly.body.velocity.x = -150;
-                fly.animations.play('flyLeft');
-            }
-            if (magic * Math.random() < 20) {
-                fly.body.velocity.y = -150;
-            } else if (magic * Math.random() < 40) {
-                fly.body.velocity.y = 150;
-            } else {
-                fly.body.velocity.y = 0;
-            }
+        }
+    },
 
-        }, this, this.frameCounter);
+    preload: function() {
+        game.stage.backgroundColor = '#80E0ED'
+        game.load.image('ground', '../assets/ground.png');
+        game.load.image('coinGold', '../assets/coinGold.png');
+        game.load.atlasJSONHash('slime', '../assets/slime.png', '../assets/slime.json');
+        game.load.atlasJSONHash('hero', '../assets/hero.png', '../assets/hero.json');
+        game.load.atlasJSONHash('fly', '../assets/fly.png', '../assets/fly.json');
+    },
+    create: function(){
+        this.initWorld();
+        this.initCoins();
+        this.initPlayer();
+        this.initCritters();
+
+        cursors = game.input.keyboard.createCursorKeys();
+
+        scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+        scoreText.fixedToCamera = true;
+    },
+    update: function() {
+        this.handleCollisions();
+        this.handlePlayerInput();
+
+        slimes.forEachAlive(this.animateSlime, this, this.updateCounter);
+        flies.forEachAlive(this.animateFly, this, this.updateCounter);
 
         game.world.wrap(player, 0, false, true, false);
-        this.frameCounter += 1;
+        this.updateCounter += 1;
     }
 };
 
